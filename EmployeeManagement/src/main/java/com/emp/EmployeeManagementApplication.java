c1fcc109317147ee6e5e63dcb76c2089deb3af61
Vamshi1311 <karrevamshi516@gmail.com>
2023-05-22 10:48:48 +0530

    First commit

--- EmployeeManagement/src/main/java/com/emp/EmployeeManagementApplication.java
+++ EmployeeManagement/src/main/java/com/emp/EmployeeManagementApplication.java
@@ -2,12 +2,17 @@ package com.emp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class EmployeeManagementApplication {
+public class EmployeeManagementApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(EmployeeManagementApplication.class);
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(EmployeeManagementApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeManagementApplication.class, args);
+    }
 }
